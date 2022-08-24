80ecad85fe3ae213d851f30ad73659a2241b249f
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-24 22:48:51 +0900

    first commit

---
+++ src/main/java/com/example/devoka/DevOkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.devoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devoka/DevOkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devoka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
