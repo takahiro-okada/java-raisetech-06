89c240d9fce71c4f4c559c31616e7bdf4f66dbf1
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-25 00:55:41 +0900

    feat:thymeleafを追加してHello Worldを表示する

--- src/main/java/com/example/devoka/controller/indexController.java
+++ src/main/java/com/example/devoka/controller/indexController.java
@@ -1,20 +1,14 @@
-package com.example.devoka.web;
+package com.example.devoka.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 //@ControllerのアノテーションをつけることでSpringにコントロールクラスと伝える
 public class indexController {
 
     @GetMapping
-    @ResponseBody
     public String index(){
-        return
-                "<h1>Hello World!</h1>" +
-                        "<h2>こんにちは</h2>" +
-                        "<style>h2{color:red;}</style>";
-
+        return "index";
     }
 }
